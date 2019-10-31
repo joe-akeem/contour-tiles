279457822335b03226f4cdd5338d6363f2b3c31f
joe-akeem <j.lengacher@gmx.net>
2019-10-31 21:23:17 +0100

    Fixed documentation service. Added a reverse proxy. Reorganized things a little.

--- build.sh
+++ build.sh
@@ -8,9 +8,13 @@ docker build -t joeakeem/tiles-builder:$TILESERVER_VERSION .
 cd ../tiles-server/
 docker build -t joeakeem/tiles-server:$TILESERVER_VERSION .
 
-cd ../docs/
+cd ../tiles-proxy/
+docker build -t joeakeem/tiles-proxy:$TILESERVER_VERSION .
+
+cd ../tiles-docs/
 docker build -t joeakeem/tiles-doc:$TILESERVER_VERSION .
 
 docker push joeakeem/tiles-builder:$TILESERVER_VERSION
 docker push joeakeem/tiles-server:$TILESERVER_VERSION
 docker push joeakeem/tiles-doc:$TILESERVER_VERSION
+docker push joeakeem/tiles-proxy:$TILESERVER_VERSION
